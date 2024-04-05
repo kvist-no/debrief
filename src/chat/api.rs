274a047274b8c75a8416d2f788502bfd720a2fad
Fredrik August Madsen-Malmo <fredrik.malmo@icloud.com>
2024-04-05 15:16:21 +0200

    get a summary from chatgpt

---
+++ src/chat/api.rs
@@ -0,0 +1,18 @@
+use chatgpt::prelude::ChatGPT;
+use anyhow::Result;
+
+pub async fn generate_brief_summary_of_pull_requests(client: ChatGPT, pull_requests: &Vec<octocrab::models::pulls::PullRequest>) -> Result<String> {
+    let mut prompt = "Please add emojis to all subsequent bullet points. You do not need to make the headings bullet points. Bold text with one set of asterisks will suffice. You will be provided with a set of pull requests that have been completed the last 24 hours. I want you to aggregate this into a bullet list highlighting the main changes. You can be a bit technical, but preferably not very. This will be posted to Slack verbatim so keep formatting light. Please attempt to group into areas of responsibility such as 'Bug fixes', 'Reliability', etc. You do not need to include any other text such as 'Here are the changes'. Try to keep the summary to at most 10 lines.\n\n".to_string();
+
+    for pull_request in pull_requests {
+        let login = pull_request.clone().user.unwrap().login.clone();
+        let title = pull_request.clone().title.clone().unwrap();
+        let body = pull_request.clone().body.unwrap_or("No body provided".to_string());
+
+        prompt += format!("User: @{login}, Pull request title: {title}, Pull request body: {body}").as_str();
+    }
+
+    let response = client.send_message(prompt).await?;
+
+    Ok(response.message().clone().content)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use octocrab::Octocrab;
 use anyhow::Result;
+use chatgpt::prelude::*;
 
 mod github;
+mod chat;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let token = read_env_var("GITHUB_TOKEN");
+    let github_token = read_env_var("GITHUB_TOKEN");
+    let instance = Octocrab::builder().personal_token(github_token).build().expect("Failed to create Octocrab instance");
 
-    let instance = Octocrab::builder().personal_token(token).build().expect("Failed to create Octocrab instance");
+    let chatgpt_token = read_env_var("OPENAI_TOKEN");
+    let chatgpt_instance = ChatGPT::new(chatgpt_token)?;
 
     let all_pull_requests = github::api::get_merged_pull_requests_from_last_day(&instance, "kvist-no", "frontend").await?;
     let filtered_pull_requests = github::api::filter_out_renovate_pull_requests(all_pull_requests);
 
-    for pull in filtered_pull_requests {
-        println!("@{} merged #{}: {}", pull.user.expect("User should be set").login, pull.number, pull.title.expect("Title should be set"));
-    }
+    let chat_response = chat::api::generate_brief_summary_of_pull_requests(chatgpt_instance, &filtered_pull_requests).await?;
+
+    println!("{}", chat_response);
 
     Ok(())
 }
